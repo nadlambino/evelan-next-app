aa9b1927d5c1265d05fb9d496ee84ec44b4b5db4
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2023-11-21 21:14:03 +0800

    ref: add a landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link"
+
 export default function Home() {
     return (
-        <main>
-        
+        <main className="main">
+            <h1>Hi, I am Ronald Lambino</h1>
+            <p>This app is for Evelan Next.js/Typescript test.</p>
+            <p>The goal is to show a list of users on initial page load.</p>
+            <p>There should be a button to retrieve more users that will append on the users list.</p>
+            <p className="my-3">I made it look like a list of active online users just to make it interesting.</p>
+            <p>Go to <Link href='/users'>/users</Link> to see the output.</p>
         </main>
     )
 }
